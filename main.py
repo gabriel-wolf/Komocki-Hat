beed3837d9247a732e30ca9fefd8ddea87b16caf
Gabriel Wolf <36235487+gabriel-wolf@users.noreply.github.com>
2018-09-01 21:05:11 -0400

    major updates and fixes on all fronts
    major updates and fixes on all fronts

--- main.py
+++ main.py
@@ -6,19 +6,30 @@ from tkinter import *
 import io
 from tkinter.ttk import Separator, Style
 
-# TODO: have music with draw
-# TODO: make draw name or something show up in top left box with joe john marry
-# FIXME: make names stop just putting themselves on top of the previous ones
-# TODO: have changeable groupnums
 
 class StudentList(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        global classname, studentnames, students, filename, groupnums, numstudents, content, classNameVar
-        filename = "Example Class.txt"
+
+        print("INIT")
+        global classname, studentnames, students, filename, groupnums, numstudents, content, classNameVar, defaultClassName, classlists, allClassesList
+        self.happened = 1
+        readDefaultClass = open("defaultClass.txt", mode='r+')
+        space_filename = readDefaultClass.readlines()
+        list_filename = []
+        for k in space_filename:
+            list_filename.append(k.strip())
+        classname = str(list_filename[0])
+        filename = classname + ".txt"
+
+        ReadAllClasses = open("allclasses.txt", mode='r+')
+        spaceAllClasses = ReadAllClasses.readlines()
+        allClassesList = []
+        for q in spaceAllClasses:
+            allClassesList.append(q.strip())
+
         groupnums = 2
         classlist = open(filename, mode='r+')
-        classname = filename[:len(filename) - 4]
         print(classname)
         space_students = classlist.readlines()
         students = []
@@ -36,21 +47,32 @@ class StudentList(tk.Frame):
         except IndexError:
             print()
 
+        self.variable = tk.StringVar()
+        self.variable.set(classname)
+
         classNameVar = StringVar()
         classNameVar.set(classname)
 
         self.create_widgets()
 
-        classname = filename[:len(filename) - 4]
-        classNameVar.set(classname)
-        print(classname)
-
 
 
     def create_widgets(self):
+
+        print(students[0])
+        for widget in self.winfo_children():
+            widget.destroy()
+
+
+
+        print(students[0])
+
         self.labelClassName = tk.Label(self, textvariable = classNameVar).grid(row = 0, column = 0, columnspan = 2)
         self.buttonAddClass = tk.Button(self, text = "Add Class", command = self.add_class_func).grid(row = 1, column = 0)
-        self.buttonChooseClass = tk.Button(self, text = "Choose Class").grid(row = 1, column = 1)
+
+        self.dropdownChooseClass = tk.OptionMenu(self, self.variable, *allClassesList, command = self.func)
+        self.dropdownChooseClass.grid(row = 1, column = 1)
+
         self.buttonDraw = tk.Button(self, text = "Draw", command = self.start_draw).grid(row = 2, column = 0, columnspan = 2, sticky = E+W)
         self.labelHeaderStudentList = tk.Label(self, text="#   Name").grid(row=3,column=0, columnspan = 2, sticky = W)
         sep = Separator(self, orient="horizontal").grid(row = 4, column = 0, columnspan = 2, sticky = E+W)
@@ -61,6 +83,7 @@ class StudentList(tk.Frame):
         random.shuffle(students)
         i = len(students)
         while i > 0:
+
             num = r -4
             self.labelStudentList = tk.Label(self, text=str(num) + ".   " + students[r-5])
             self.labelStudentList.grid(row=r,column=0, columnspan = 2, sticky = W)
@@ -70,6 +93,35 @@ class StudentList(tk.Frame):
         self.buttonShuffle = tk.Button(self, text = "Shuffle", command = self.create_widgets).grid(row = len(students)+5, column = 0, columnspan = 2, sticky = E+S+W)
 
 
+    def func(self,value):
+        i = 0
+        classname = value
+        classNameVar.set(classname)
+        filename = classname + ".txt"
+        classlist = open(filename, mode='r+')
+        print(classname)
+        space_students = classlist.readlines()
+        students_not = []
+        for cocopuffs in space_students:
+            students_not.append(cocopuffs.strip())
+
+        del students[:]
+        try:
+            while i >= 0:
+                students.append(students_not[i])
+                i = i + 1
+        except IndexError:
+            print()
+
+        ReadAllClasses = open("allclasses.txt", mode='r+')
+        spaceAllClasses = ReadAllClasses.readlines()
+        allClassesList = []
+        for q in spaceAllClasses:
+            allClassesList.append(q.strip())
+
+        self.happened = 1
+        self.create_widgets()
+
 
     def add_class_func(self):
         def grabText(event):
@@ -78,9 +130,15 @@ class StudentList(tk.Frame):
             filename = str(ClassEntryBox.get())
             classname = filename
             classNameVar.set(classname)
-            # # FIXME: make class name become text file name
-            # # FIXME: have it create text file
             full_file_name = filename + ".txt"
+
+            with open("allclasses.txt", mode='r+') as classesFile:
+                for line in classesFile:
+                    if classname in line:
+                        break
+                    else:
+                        classesFile.write(classname)
+
             classlist = open(full_file_name, mode='w+')
             #classname = filename[:len(filename) - 4]
             classlist.write(input)
@@ -134,64 +192,76 @@ class StudentList(tk.Frame):
 
 
     def start_draw(self):
-        t = tk.Toplevel(self)
-        t.resizable(True,True)
-        t.grid()
-        t.config()
-        #t.geometry('500x500')
-        t.wm_title("Draw")
-
-        # groupnums / len(students) + 1 = # r
-        # chane of groupnums = run startdraw again
-        # but what if starts drawing again? okay
-
-
-
-
-        #t.frame = tk.Frame(t, relief=RAISED, borderwidth=1).grid(sticky = N+W, row = 0, column = 0, columnspan = 4, rowspan = 4)
-        t.names_draw = tk.Label(t, text = "John Joe Mary", width = 25).grid(sticky = N+W, row = 0, column = 0, columnspan = 10, rowspan = 4)
-
-        #Names = tk.Label(t, text = "ME ME ME!", justify = CENTER).grid(sticky = N+E+S+W)
-        #frame.pack(fill=BOTH, side = BOTTOM, expand=True)
+        if self.happened == 1 :
+            print("starting initial draw")
+            global t
+            t = tk.Toplevel()
+            t.resizable(True,True)
+            t.grid()
+            t.config()
+            t.wm_title("Draw")
+            self.happened = 0
+        else:
+            print()
 
 
-        #l = tk.Label(t, text="IT WORKS!").grid(row = 0, column = 0)
-        t.labelHeaderStudentList = tk.Label(t, text="#   Name", width = 20).grid(row=0,column=9, columnspan = 4, sticky=W)
-        t.sep = Separator(t, orient="horizontal").grid(row = 1, column = 9, columnspan = 8, sticky = E+W)
-        t.sep2 = Separator(t, orient="vertical").grid(row = 0, column = 9, rowspan = 8, sticky = N+S)
+        names_draw = tk.Label(t, text = "John Joe Mary", width = 25).grid(sticky = N+W, row = 0, column = 0, columnspan = 10, rowspan = 4)
+
+        labelHeaderStudentList = tk.Label(t, text="#   Name", width = 20).grid(row=0,column=9, columnspan = 4, sticky=W)
+        sep = Separator(t, orient="horizontal").grid(row = 1, column = 9, columnspan = 8, sticky = E+W)
+        sep2 = Separator(t, orient="vertical").grid(row = 0, column = 9, rowspan = 8, sticky = N+S)
         sty = Style(t)
         sty.configure("TSeperator", fg = "black")
-        r = 1
+        r = 3
+        grouplabelsrepeat = groupnums - 1
         numstudents = len(students)
         random.shuffle(students)
 
-        # FIXME: want every time press draw names to add one more line to the names list
-        # FIXME: eventually want names being drawn to be shown where Joe John Mary is
+        studentlabelstringvar1 = StringVar()
+        groupsub = groupnums - 1
+        groupsub2 = groupnums - 1
+        def chunks(l, n):
+            for i in range(0, len(l), n):
+                yield l[i:i+n]
+        print(list(chunks(students, groupnums)))
+        numgrounps = len(list(chunks(students, groupnums)))
+        groupsub3 = numgrounps - 1
+        labelendlist = range(0,numgrounps+1)
 
-        #drawnames()
+        num = 1
+        options = []
+
+        for i in range(numgrounps):
+            # for i in range(groupnums+1):
+            new_label_value = str(num) + ". "
+            print(str(num) + ". ", end = '', flush = True)
+            for i in range(groupnums):
+                try:
+                    new_label_value = new_label_value + list(chunks(students, groupnums))[groupsub3][groupsub2]
+                    print(list(chunks(students, groupnums))[groupsub3][groupsub2], end='', flush = True)
+                    if groupsub2 != 0:
+                        new_label_value = new_label_value + " & "
+                        print(" & ", end = '', flush = True)
+                    else:
+                        options.append(new_label_value)
+                        studentlabelstringvar1.set(new_label_value)
+                        print("options: " + options[0])
+                        print(new_label_value)
+                        print(studentlabelstringvar1)
+                        tk.Label(t, text=new_label_value, width = 30).grid(row=r+1,column=10, columnspan = 4, sticky=W)
+
+                except IndexError:
+                    print('', end = '', flush = True)
+                groupsub2 = groupsub2 - 1
+                r = r + groupnums
+            num = num + 1
+            groupsub3 = groupsub3 - 1
+            groupsub2 = groupnums - 1
 
 
-        # def drawnames():
-        try:
-            num = r
-            for i in range(int(len(students)/groupnums)-1, -1, -1):
+        sep3 = Separator(t, orient="horizontal").grid(row = r+2, column = 0, columnspan = 20, sticky = E+W)
 
-                # FIXME: have align left for all
-                t.labelStudentList = tk.Label(t, text=str(num) + ".   " + students[r-1] + " & " + students[r], width = 20).grid(row=r+1,column=10, columnspan = 4, sticky=W)
-                # t.labelStudentList2 = tk.Label(t, text=" & ").grid(row=r,column=10)
-                # t.labelStudentList3 = tk.Label(t, text=students[r]).grid(row=r,column=11)
-
-                num = num + 1
-                numstudents = numstudents - 1
-                r = r + 2
-                if students[i] == 0:
-                    students.pop(i)
-        except IndexError:
-            print()
-
-        t.sep3 = Separator(t, orient="horizontal").grid(row = r+2, column = 0, columnspan = 20, sticky = E+W)
-
-        drawnamesButton = tk.Button(t, text = "Draw Names").grid(row = r+3, column = 0, padx = 10, pady = 10, ipadx = 5, ipady = 5)
+        drawnamesButton = tk.Button(t, text = "Draw Groups", command = self.start_draw).grid(row = r+3, column = 0, padx = 10, pady = 10, ipadx = 5, ipady = 5)
 
 
 
@@ -201,6 +271,7 @@ if __name__ == '__main__':
     root.title("Komocki Hat")
     root.resizable(True,True)
     studentList = StudentList(root)
+
     studentList.grid()
     studentList.config()
     root.mainloop()
--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-s = ['hello\n', 'friend\n', 'my\n']
-
-final_list = []
-for i in s:
-    final_list.append(i.strip())
-
-
-
-print(final_list[0])
-print(s[1])
-print(s[2])
+a = [1, 2, 3]
+for x in a:
+    print(x)
